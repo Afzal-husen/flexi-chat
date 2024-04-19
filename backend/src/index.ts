4e9dd5b1e4580f572aba704a63aa5f96971e9a27
Afzal Diwan <afzalhusen3@gmail.com>
2024-04-19 18:23:52 +0530

    feat:(add socket.io): make front end connect to backend usinf socket.io

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,11 +11,16 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
   console.log(`socket ${socket.id} connected`);
+  socket.on("chat message", (message) => {
+    console.log(`${socket.id} has send ${message}`);
+    socket.broadcast.emit("chat message", message);
+  });
 });
 
 server.listen(5000, () => console.log("server running at port 5000"));
--- frontend/src/components/page/home/chat-box.tsx
+++ frontend/src/components/page/home/chat-box.tsx
@@ -2,12 +2,30 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { socket } from "@/utils/socket";
 import { SendHorizonal } from "lucide-react";
-import React from "react";
+import { useEffect, useState } from "react";
 
 const ChatBox = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+  console.log({ messages });
+
+  useEffect(() => {
+    socket.on("chat message", (msg) => {
+      setMessages([...messages, msg]);
+    });
+    return () => {
+      socket.off("chat message");
+    };
+  }, [ messages]);
+
   const sendMessage = (e: React.FormEvent) => {
     e.preventDefault();
+    if (message) {
+      socket.connect();
+      socket.emit("chat message", message);
+    }
   };
   return (
     <div className="bg-primary-foreground flex-grow flex flex-col">
@@ -15,7 +33,10 @@ const ChatBox = () => {
       <form
         className="bg-white py-3 px-5 flex items-center gap-x-5 border-t-[1px]"
         onSubmit={sendMessage}>
-        <Input className="bg-zinc-100 border-none" />
+        <Input
+          className="bg-zinc-100 border-none"
+          onChange={(e) => setMessage(e.target.value)}
+        />
         <Button variant={"ghost"} className="p-2 w-fit h-fit">
           <SendHorizonal className="size-5 text-teal-600" />
         </Button>
